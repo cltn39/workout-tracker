ab39f8bb85b0cfa0c605ba0da89af538771f2a98
Chisoo Song <cltn39@gmail.com>
2021-02-20 10:59:47 -0800

    Added models & routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,7 +20,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
